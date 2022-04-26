8d0e733488331834c33469739ac2af12b08d88fc
MaxwellDokyunKim <101620585+MaxwellDokyunKim@users.noreply.github.com>
2022-04-26 09:33:02 +0900

    Add files via upload

---
+++ FineDust/FineDust/SubDlg1.cpp
@@ -0,0 +1,129 @@
+﻿// SubDlg1.cpp: 구현 파일
+//
+
+#include "pch.h"
+#include "FineDust.h"
+#include "SubDlg1.h"
+#include "afxdialogex.h"
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <string>
+#include "City.h"
+#include "framework.h"
+#include "ChartCtrl_source_cord/ChartCtrl.h"
+using namespace std;
+
+Datas DT3;
+
+// CSubDlg1 대화 상자
+
+IMPLEMENT_DYNAMIC(CSubDlg1, CDialogEx)
+
+CSubDlg1::CSubDlg1(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_MYSUBDIALOG1, pParent)
+	
+	
+	, m_GetCity(_T(""))
+	
+{
+
+}
+
+CSubDlg1::~CSubDlg1()
+{
+}
+
+void CSubDlg1::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+	DDX_Control(pDX, IDC_LIST1, m_FD25List1);
+	DDX_Control(pDX, IDC_LIST2, m_FD25List2);
+	//  DDX_Control(pDX, IDC_EDIT3, m_GetCity);
+	//  DDX_Control(pDX, IDC_EDIT1, m_GetString2);
+
+
+	DDX_Text(pDX, IDC_EDIT3, m_GetCity);
+}
+
+
+BEGIN_MESSAGE_MAP(CSubDlg1, CDialogEx)
+	ON_BN_CLICKED(IDC_BUTTON1, &CSubDlg1::OnBnClickedButton1)
+	ON_BN_CLICKED(IDC_RADIO2, &CSubDlg1::OnBnClickedRadio2)
+END_MESSAGE_MAP()
+
+
+// CSubDlg1 메시지 처리기
+
+
+void CSubDlg1::OnBnClickedButton1()
+{
+	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+
+	m_FD25List2.DeleteAllItems();
+	m_FD25List2.DeleteColumn(100);
+
+
+	m_FD25List2.InsertColumn(0, _T("측정유형"), LVCFMT_CENTER, 70);
+	m_FD25List2.InsertColumn(1, _T("시.도별"), LVCFMT_CENTER, 70);
+	m_FD25List2.InsertColumn(2, _T("시.군.구별(1)"), LVCFMT_CENTER, 100);
+	m_FD25List2.InsertColumn(3, _T("시.군.구별(2)"), LVCFMT_CENTER, 100);
+	m_FD25List2.InsertColumn(4, _T("2019.12"), LVCFMT_CENTER, 75);
+	m_FD25List2.InsertColumn(5, _T("2020.01"), LVCFMT_CENTER, 75);
+	m_FD25List2.InsertColumn(6, _T("2020.02"), LVCFMT_CENTER, 75);
+	m_FD25List2.InsertColumn(7, _T("2020.03"), LVCFMT_CENTER, 75);
+	m_FD25List2.InsertColumn(8, _T("2020.04"), LVCFMT_CENTER, 75);
+	m_FD25List2.InsertColumn(9, _T("2020.05"), LVCFMT_CENTER, 75);
+
+	m_FD25List2.SetExtendedStyle(m_FD25List2.GetExtendedStyle() | LVS_EX_FULLROWSELECT | LVS_EX_GRIDLINES);
+	int i = 0;
+
+	string getcido;
+	ifstream data;
+	data.open("finedust2.5s.csv"); //input file stream 디스크 경로를 주면 읽어주는 것.
+
+	string line;
+	UpdateData(true);
+	
+	while (std::getline(data, line)) 
+	{
+		stringstream lineStream(line); 
+		string cell;
+		int num = 0;
+		int j = 0;
+		int count = m_FD25List2.GetItemCount();
+		getcido = string(CT2CA(m_GetCity));
+		size_t found = line.find(getcido);
+		if (found != string::npos)
+		{
+			while (std::getline(lineStream, cell, ',')) 
+			{
+				if (j == 0)
+				{
+					DT3.EXData[i][j] = cell.c_str();
+					m_FD25List2.InsertItem(count, DT3.EXData[i][j]);
+				}
+				else if (j > 0)
+				{
+					DT3.EXData[i][j] = cell.c_str();
+					m_FD25List2.SetItem(count, j, LVIF_TEXT, DT3.EXData[i][j], 0, 0, 0, 0);
+				}
+				
+				j++;
+			}
+			i++;
+
+		}
+	}
+	
+	UpdateData(false);
+	data.close();
+	
+
+}
+
+
+void CSubDlg1::OnBnClickedRadio2()
+{
+	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+}
---
+++ FineDust/FineDust/resource.h
@@ -0,0 +1,47 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// FineDust.rc에서 사용되고 있습니다.
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_FINEDUST_DIALOG             102
+#define IDR_MAINFRAME                   128
+#define IDD_MYSUBDIALOG1                130
+#define IDD_MYSUBDIALOG2                132
+#define IDD_MYCIDODIALOG1               134
+#define IDC_LIST1                       1000
+#define IDC_LIST2                       1001
+#define IDC_MYSTATIC                    1002
+#define IDC_RADIO1                      1003
+#define IDC_RADIO2                      1004
+#define IDC_EDIT1                       1005
+#define IDC_BUTTON1                     1006
+#define IDC_BUTTON2                     1007
+#define IDC_PIC1                        1008
+#define IDC_MYSTATIC2                   1009
+#define IDC_MYSTATIC3                   1010
+#define IDC_MYSTATIC4                   1011
+#define IDC_MYSTATIC5                   1012
+#define IDC_MYSTATIC6                   1013
+#define IDC_MYSTATIC7                   1014
+#define IDC_MYSTATIC8                   1015
+#define IDC_MYSTATIC9                   1016
+#define IDC_MYGRAPH1                    1019
+#define IDC_MYGRAPH2                    1020
+#define IDC_MYGRAPTHMAIN1               1022
+#define IDC_MYGRAPH5                    1022
+#define IDC_MYGRAPH6                    1024
+#define IDC_MYSYSLINK2                  1026
+#define IDC_EDIT3                       1029
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        136
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1030
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
